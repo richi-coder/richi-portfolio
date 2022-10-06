76eeffcd01de11d64bd398ada4810fa3dc254b2b
Richi <113008174+richiProgrammer@users.noreply.github.com>
2022-10-06 03:16:24 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+let btn = document.querySelector("#show-all");
+let project = document.querySelectorAll(".project");
+let projectImage = document.querySelectorAll(".project-image");
+let paragraph = document.querySelectorAll(".paragraph");
+let shots = document.querySelector(".shots");
+let logo = document.querySelector(".logo");
+let icon = document.querySelector("#mobilebutton");
+let mobileMenu = document.querySelector("#mobilemenu");
+
+icon.onclick= () => {
+    icon.classList.toggle("fa-close");
+    icon.classList.toggle("fa-bars");
+    if (icon.classList.contains("fa-close")) {
+        mobileMenu.style.display = "block";
+        mobileMenu.style.visibility = "visible";
+    } else {
+        mobileMenu.style.display = "none";
+        mobileMenu.style.visibility = "hidden";
+    }
+    mobileMenu.classList.toggle("reveal");
+}
+
+logo.onclick = () => location.reload();
+
+window.addEventListener("scroll", () => {
+    if (document.documentElement.clientWidth < 920) {  // MOBILE
+        if (document.documentElement.scrollTop > 180 && document.documentElement.scrollTop <= 220) {
+            console.log("animate top projects")
+            document.getElementById("top-projects").classList.add("shine");
+        }
+        if (document.documentElement.scrollTop > 220 && document.documentElement.scrollTop <= 670) {
+            document.getElementsByClassName("project")[0].classList.add("right-reveal");
+        }
+        if (document.documentElement.scrollTop > 670 && document.documentElement.scrollTop <= 1170) {
+            document.getElementsByClassName("project")[1].classList.add("left-reveal");
+        }      
+        if (document.documentElement.scrollTop > 1170 && document.documentElement.scrollTop <= 1600) {
+            document.getElementsByClassName("project")[2].classList.add("right-reveal");
+        }  
+        if (document.documentElement.scrollTop > 1600) {
+            document.getElementsByClassName("project")[3].classList.add("left-reveal");
+        }  
+    }
+    else { // DESKTOP OR LARGE SCREENS
+        console.log(document.documentElement.scrollTop)
+        if (document.documentElement.scrollTop > 160 && document.documentElement.scrollTop <= 180) {
+            console.log("animate top projects")
+            document.getElementById("top-projects").classList.add("shine");
+        }
+        if (document.documentElement.scrollTop > 180 && document.documentElement.scrollTop <= 650) {
+            document.getElementsByClassName("project")[0].classList.add("right-reveal");
+            document.getElementsByClassName("project")[1].classList.add("right-reveal");
+        }
+        if (document.documentElement.scrollTop > 650 && document.documentElement.scrollTop <= 1120) {
+            document.getElementsByClassName("project")[2].classList.add("left-reveal");
+            document.getElementsByClassName("project")[3].classList.add("left-reveal");
+        } 
+    }
+})
